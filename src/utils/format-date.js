12d03f54b4602f031c7693b08ae7f8743f54c071
Maksym Shcherban <peregrin@ukr.net>
2021-10-03 18:20:27 +0200

    add console log item

---
+++ src/utils/format-date.js
@@ -0,0 +1,13 @@
+export default function(d) {
+  if (!d) {
+    return '';
+  }
+  return (
+    d.getFullYear() + "-" +
+    ("00" + (d.getMonth() + 1)).slice(-2) + "-" +
+    ("00" + d.getDate()).slice(-2) + " " +
+    ("00" + d.getHours()).slice(-2) + ":" +
+    ("00" + d.getMinutes()).slice(-2) + ":" +
+    ("00" + d.getSeconds()).slice(-2)
+  );
+}
